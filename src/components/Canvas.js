779a208e0eadc365ec2fe8972991caa699b8c1a7
Daniel Goldman <dzgoldman@wesleyan.edu>
2018-01-18 12:33:54 -0500

    success porting modified scroller  component into react build, halleluya

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,31 +1,278 @@
 import React, { PureComponent } from 'react'
 // import $ from "jquery";
 import autoBind from 'react-autobind';
+import '../utils/Animate'
+
+import Scroller from '../utils/Scroller'
+import Tiling from '../utils/Tiling'
 
 class Canvas extends PureComponent {
   constructor(props) {
     super(props)
-    
-    this.state = {
-      test: '',
-      pixelSize: 10,
-      width: 100,
-    }
 
-    this.height = this.state.width ;
+
     autoBind(this);
   }
 
+  componentDidMount(){
+
+	// Settings
+	var contentWidth = 2000;
+	var contentHeight = 2000;
+	var cellWidth = 10;
+	var cellHeight = 10;
+	var pixelSize = cellWidth
+	
+	var content = document.getElementById('content');
+	content.addEventListener("mousedown", handleMouseDown, false); 
+	var context = content.getContext('2d');
+	var tiling = new Tiling;
+	
+	function handleMouseDown(e) {
+		var ctx = context
+		var pixel = getPixelSelected(e)
+		ctx.fillStyle = 'rgba(0,0,255,255)'
+		console.log(pixel)
+		// ctx.fillRect(pixel['x'], pixel['y'], pixelSize, pixelSize);
+		newCellHash[`${pixel['x']},${pixel['y']}`] = 'rgba(0,0,255,255)'
+	}
+
+function getPixelSelected(e) {
+	const values = scroller.getValues()
+	const zoomLevel = values.zoom;
+	const left = values.left
+	const top = values.top
+	console.log(zoomLevel)
+	var canvas = content
+        var rect = canvas.getBoundingClientRect(),
+			pixel = new Array;
+			console.log(rect)
+        
+        pixel['x'] = Math.floor( ((e.clientX + left- rect.left)/pixelSize )/ zoomLevel ) ;
+              pixel['y'] = Math.floor( ((e.clientY + top - rect.top)/pixelSize )/zoomLevel ); 
+        
+        return pixel;
+      
+      }
+
+
+	// Canvas renderer
+	var render = function(left, top, zoom) {
+		
+		// Sync current dimensions with canvas
+		content.width = clientWidth;
+		content.height = clientHeight;
+		
+		// Full clearing
+		context.clearRect(0, 0, clientWidth, clientHeight);
+
+		// Use tiling
+		tiling.setup(clientWidth, clientHeight, contentWidth, contentHeight, cellWidth, cellHeight);
+		tiling.render(left, top, zoom, paint);
+	};
+	
+	var newCellHash = {
+
+	}
+	// Cell Paint Logic
+	var paint = function(col, row, left, top, width, height, zoom) {
+
+		context.fillStyle = row%2 + col%2 > 0 ? "#ddd" : "red";
+		var c = newCellHash[`${row},${col}`]
+		if (c ){
+			context.fillStyle = newCellHash[`${row},${col}`]
+		}
+		context.fillRect(left, top, width, height);
+		
+		// context.fillStyle = "black";
+		// context.font = (14 * zoom).toFixed(2) + 'px "Helvetica Neue", Helvetica, Arial, sans-serif';
+		
+		// Pretty primitive text positioning :)
+		// context.fillText(row + "," + col, left + (6 * zoom), top + (18 * zoom));
+		
+	};
+	
+
+
+
+
+	// Intialize layout
+var container = document.getElementById("container");
+var content = document.getElementById("content");
+var clientWidth = 0;
+var clientHeight = 0;
+
+// Initialize Scroller
+var scroller = new Scroller(render, {
+	zooming: true
+});
+
+
+var scrollLeftField = document.getElementById("scrollLeft");
+var scrollTopField = document.getElementById("scrollTop");
+var zoomLevelField = document.getElementById("zoomLevel");
+
+setInterval(function() {
+	var values = scroller.getValues();
+	scrollLeftField.value = values.left.toFixed(2);
+	scrollTopField.value = values.top.toFixed(2);
+	zoomLevelField.value = values.zoom.toFixed(2);
+}, 500);
+
+
+var rect = container.getBoundingClientRect();
+scroller.setPosition(rect.left + container.clientLeft, rect.top + container.clientTop);
+
+
+// Reflow handling
+var reflow = function() {
+	clientWidth = container.clientWidth;
+	clientHeight = container.clientHeight;
+	scroller.setDimensions(clientWidth, clientHeight, contentWidth, contentHeight);
+};
+
+window.addEventListener("resize", reflow, false);
+reflow();
+
+var checkboxes = document.querySelectorAll("#settings input[type=checkbox]");
+for (var i=0, l=checkboxes.length; i<l; i++) {
+	checkboxes[i].addEventListener("change", function() {
+		scroller.options[this.id] = this.checked;
+	}, false);
+}
+
+document.querySelector("#settings #zoom").addEventListener("click", function() {
+	scroller.zoomTo(parseFloat(document.getElementById("zoomLevel").value));
+}, false);
+
+document.querySelector("#settings #zoomIn").addEventListener("click", function() {
+	scroller.zoomBy(1.2, true);
+}, false);
+
+document.querySelector("#settings #zoomOut").addEventListener("click", function() {
+	scroller.zoomBy(0.8, true);
+}, false);
+
+document.querySelector("#settings #scrollTo").addEventListener("click", function() {
+	scroller.scrollTo(parseFloat(document.getElementById("scrollLeft").value), parseFloat(document.getElementById("scrollTop").value), true);
+}, false);
+
+document.querySelector("#settings #scrollByUp").addEventListener("click", function() {
+	scroller.scrollBy(0, -150, true);
+}, false);
+
+document.querySelector("#settings #scrollByRight").addEventListener("click", function() {
+	scroller.scrollBy(150, 0, true);
+}, false);
+
+document.querySelector("#settings #scrollByDown").addEventListener("click", function() {
+	scroller.scrollBy(0, 150, true);
+}, false);
+
+document.querySelector("#settings #scrollByLeft").addEventListener("click", function() {
+	scroller.scrollBy(-150, 0, true);
+}, false);
+
+
+if ('ontouchstart' in window) {
+
+	container.addEventListener("touchstart", function(e) {
+		// Don't react if initial down happens on a form element
+		if (e.touches[0] && e.touches[0].target && e.touches[0].target.tagName.match(/input|textarea|select/i)) {
+			return;
+		}
+
+		scroller.doTouchStart(e.touches, e.timeStamp);
+		e.preventDefault();
+	}, false);
+
+	document.addEventListener("touchmove", function(e) {
+		scroller.doTouchMove(e.touches, e.timeStamp, e.scale);
+	}, false);
+
+	document.addEventListener("touchend", function(e) {
+		scroller.doTouchEnd(e.timeStamp);
+	}, false);
+
+	document.addEventListener("touchcancel", function(e) {
+		scroller.doTouchEnd(e.timeStamp);
+	}, false);
+
+} else {
+
+	var mousedown = false;
+
+	container.addEventListener("mousedown", function(e) {
+		if (e.target.tagName.match(/input|textarea|select/i)) {
+			return;
+		}
+		
+		scroller.doTouchStart([{
+			pageX: e.pageX,
+			pageY: e.pageY
+		}], e.timeStamp);
+
+		mousedown = true;
+	}, false);
+
+	document.addEventListener("mousemove", function(e) {
+		if (!mousedown) {
+			return;
+		}
+		
+		scroller.doTouchMove([{
+			pageX: e.pageX,
+			pageY: e.pageY
+		}], e.timeStamp);
+
+		mousedown = true;
+	}, false);
+
+	document.addEventListener("mouseup", function(e) {
+		if (!mousedown) {
+			return;
+		}
+		
+		scroller.doTouchEnd(e.timeStamp);
+
+		mousedown = false;
+	}, false);
+
+	container.addEventListener(navigator.userAgent.indexOf("Firefox") > -1 ? "DOMMouseScroll" :  "mousewheel", function(e) {
+		scroller.doMouseZoom(e.detail ? (e.detail * -120) : e.wheelDelta, e.timeStamp, e.pageX, e.pageY);
+	}, false);
+
+}
+
+  }
 
   render() {
     return (
       <div>
-        {this.renderPallate()}
-      <canvas id='can' onClick={this.draw} ref={(canvas)=> this.canvas = canvas } width={this.state.width} height={this.state.width}>
-      </canvas>
-      <div onClick={this.test}>test</div>
-      <div onClick={this.test2}>test2</div>
-      <div>size{this.state.pixelSize}</div>
+     <div id="container">
+     <canvas id="content"></canvas>
+ </div>
+ 
+ <div id="settings">
+     <div><label for="scrollingX">ScrollingX: </label><input type="checkbox" id="scrollingX" checked/></div>
+     <div><label for="scrollingY">ScrollingY: </label><input type="checkbox" id="scrollingY" checked/></div>
+     <div><label for="animating">Animating: </label><input type="checkbox" id="animating" checked/></div>
+     <div><label for="bouncing">Bouncing: </label><input type="checkbox" id="bouncing" checked/></div>
+     <div><label for="locking">Locking: </label><input type="checkbox" id="locking" checked/></div>
+
+     <div><label for="zooming">Zooming: </label><input type="checkbox" id="zooming" checked/></div>
+     <div><label for="minZoom">Min Zoom: </label><input type="text" id="minZoom" size="5" value="0.5"/></div>
+     <div><label for="maxZoom">Max Zoom: </label><input type="text" id="maxZoom" size="5" value="3"/></div>
+     <div><label for="zoomLevel">Zoom Level: </label><input type="text" id="zoomLevel" size="5"/></div>
+     <div><button id="zoom">Zoom to Level</button><button id="zoomIn">+</button><button id="zoomOut">-</button></div>
+     
+     <div><label for="scrollLeft">Scroll Left: </label><input type="text" id="scrollLeft" size="9"/></div>
+     <div><label for="scrollTop">Scroll Top: </label><input type="text" id="scrollTop" size="9"/></div>
+     <div><button id="scrollTo">Scroll to Coords</button></div>
+
+     <div><button id="scrollByUp">&uarr;</button><button id="scrollByDown">&darr;</button><button id="scrollByLeft">&larr;</button><button id="scrollByRight">&rarr;</button></div>
+ </div>
+ 
       </div>
     );
   }
--- src/components/CanvasContainer.js
+++ src/components/CanvasContainer.js
@@ -17,160 +17,158 @@ class CanvasContainer extends PureComponent {
     this.height = this.state.width ;
     autoBind(this);
   }
-  test(){
-    // this.saveDrawing()
-    this.getDrawing((result)=>{
-      console.log(typeof result)
-      console.log('????', result.slice(2))
-      var x = enc.decryptMain(result.slice(2))
-      // x = enc.zoom(x, this.state.pixelSize)
-      this.applyDrawing(x)
-    })
-    // const d = this.context.getImageData(0,0,this.state.width, this.state.width).data
-    // console.log(d)
-    // var r = enc.clampedArrToNumArr(d)
-    // console.log(r)
-    // this.applyDrawing('222222222')
-    // this.context.scale(2, 2) 
-    // this.setState({
-    //   pixelSize: 2* this.state.pixelSize,
-    //   width: 2 * this.state.width
-    // })
-    // this.zoomIn()
-  }
-  test2(){
-        this.saveDrawing()
+//   test(){
+//     // this.saveDrawing()
+//     this.getDrawing((result)=>{
+//       console.log(typeof result)
+//       console.log('????', result.slice(2))
+//       var x = enc.decryptMain(result.slice(2))
+//       // x = enc.zoom(x, this.state.pixelSize)
+//       this.applyDrawing(x)
+//     })
+//     // const d = this.context.getImageData(0,0,this.state.width, this.state.width).data
+//     // console.log(d)
+//     // var r = enc.clampedArrToNumArr(d)
+//     // console.log(r)
+//     // this.applyDrawing('222222222')
+//     // this.context.scale(2, 2) 
+//     // this.setState({
+//     //   pixelSize: 2* this.state.pixelSize,
+//     //   width: 2 * this.state.width
+//     // })
+//     // this.zoomIn()
+//   }
+//   test2(){
+//         this.saveDrawing()
 
-  }
+//   }
 
-  zoomIn(){
-    const {pixelSize, width} = this.state
-    const d = this.context.getImageData(0,0,this.state.width, this.state.width).data
-    const numArray = enc.clampedArrToNumArr(d)
-    var scale = 3
-    const zoomedStr = enc.zoom(numArray, 3)
-    this.setState({
-      pixelSize: 3  * pixelSize,
-      width: width * 3
-    })
-    setTimeout(()=>{this.applyDrawing(zoomedStr), 2000});
+//   zoomIn(){
+//     const {pixelSize, width} = this.state
+//     const d = this.context.getImageData(0,0,this.state.width, this.state.width).data
+//     const numArray = enc.clampedArrToNumArr(d)
+//     var scale = 3
+//     const zoomedStr = enc.zoom(numArray, 3)
+//     this.setState({
+//       pixelSize: 3  * pixelSize,
+//       width: width * 3
+//     })
+//     setTimeout(()=>{this.applyDrawing(zoomedStr), 2000});
 
-  }
-  componentDidMount(){
-    this.context = this.canvas.getContext("2d");
-    this.setColor('rgba(0,0,255,255)')
+//   }
+//   componentDidMount(){
+//     this.context = this.canvas.getContext("2d");
+//     this.setColor('rgba(0,0,255,255)')
 
-  }
+//   }
 
-  saveDrawing(options){
+//   saveDrawing(options){
     
 
-    const {width, pixelSize} = this.state
-    const {inkTokenInstance, currentUser} = this.props
-    var method
+//     const {width, pixelSize} = this.state
+//     const {inkTokenInstance, currentUser} = this.props
+//     var method
 
-    // switch (options.method) {
-    //   case 'string':
+//     // switch (options.method) {
+//     //   case 'string':
         
-    //     break;
+//     //     break;
     
-    //   default:
-    //     method = 
-    //     break;
-    // }
+//     //   default:
+//     //     method = 
+//     //     break;
+//     // }
     
-     const d = this.context.getImageData(0,0,width, width).data
-    // console.log(d)
-    var r = enc.clampedArrToNumArr(d)
-    // r = enc.zoom(r, 1/pixelSize)
-    const encryptedData = enc.encryptMain(r)
-    inkTokenInstance.drawBytes('0x' + encryptedData , {from: currentUser})
-     .then( async (result) => {
-      // Get the value from the contract to prove it worked.
-      this.getDrawing()
-    })
-  }
+//      const d = this.context.getImageData(0,0,width, width).data
+//     // console.log(d)
+//     var r = enc.clampedArrToNumArr(d)
+//     // r = enc.zoom(r, 1/pixelSize)
+//     const encryptedData = enc.encryptMain(r)
+//     inkTokenInstance.drawBytes('0x' + encryptedData , {from: currentUser})
+//      .then( async (result) => {
+//       // Get the value from the contract to prove it worked.
+//       this.getDrawing()
+//     })
+//   }
 
-  getDrawing(next){
-    const {inkTokenInstance, currentUser} = this.props
+//   getDrawing(next){
+//     const {inkTokenInstance, currentUser} = this.props
 
-    inkTokenInstance.getCanvasBytes.call({from: currentUser}).then((result)=>{
-      console.log('?current canvas:', result)
-      next && next(result)
-    })
-  }
+//     inkTokenInstance.getCanvasBytes.call({from: currentUser}).then((result)=>{
+//       console.log('?current canvas:', result)
+//       next && next(result)
+//     })
+//   }
 
- getPixelSelected(e) {
-  var rect = this.canvas.getBoundingClientRect(),
-      pixel = new Array;
+//  getPixelSelected(e) {
+//   var rect = this.canvas.getBoundingClientRect(),
+//       pixel = new Array;
   
-  pixel['x'] = Math.floor((e.clientX - rect.left)/this.state.pixelSize) * this.state.pixelSize;
-	pixel['y'] = Math.floor((e.clientY - rect.top)/this.state.pixelSize) * this.state.pixelSize;
+//   pixel['x'] = Math.floor((e.clientX - rect.left)/this.state.pixelSize) * this.state.pixelSize;
+// 	pixel['y'] = Math.floor((e.clientY - rect.top)/this.state.pixelSize) * this.state.pixelSize;
   
-  return pixel;
+//   return pixel;
 
-}
+// }
 
- getColor(e) {
-    return this.context.fillStyle
+//  getColor(e) {
+//     return this.context.fillStyle
 
-//   context.fillStyle = 'document.defaultView.getComputedStyle(e.target,null)['backgroundColor']';
-}
+// //   context.fillStyle = 'document.defaultView.getComputedStyle(e.target,null)['backgroundColor']';
+// }
 
-setColor(color){
-  // console.log(color)
-  this.context.fillStyle = color
-}
+// setColor(color){
+//   // console.log(color)
+//   this.context.fillStyle = color
+// }
 
-  draw(e){
-    const pixel = this.getPixelSelected(e);
-    // this.getColor()
-    this.context.fillRect(pixel['x'], pixel['y'], this.state.pixelSize, this.state.pixelSize);
+//   draw(e){
+//     const pixel = this.getPixelSelected(e);
+//     // this.getColor()
+//     this.context.fillRect(pixel['x'], pixel['y'], this.state.pixelSize, this.state.pixelSize);
 
-    // for( var i = 0; i < 5000; i++){
-    //   this.context.fillRect(i, i, this.state.pixelSize, this.state.pixelSize);
-    // }
-  }
+//     // for( var i = 0; i < 5000; i++){
+//     //   this.context.fillRect(i, i, this.state.pixelSize, this.state.pixelSize);
+//     // }
+//   }
 
-  applyDrawing(numStr){
-    console.log('??', numStr)
-    // TODO: start at root coordinate, width is width of drawing instead of whole canvas
-    const width = Math.sqrt(numStr.length);
-    for (var i = 0; i < width; i++){
-      for (var j=0; j<width; j++){
-        const currentColorNum = numStr[i + (width * j)];
-        const currentColor = `rgba(${enc.numToRbga[currentColorNum] })`
+//   applyDrawing(numStr){
+//     console.log('??', numStr)
+//     // TODO: start at root coordinate, width is width of drawing instead of whole canvas
+//     const width = Math.sqrt(numStr.length);
+//     for (var i = 0; i < width; i++){
+//       for (var j=0; j<width; j++){
+//         const currentColorNum = numStr[i + (width * j)];
+//         const currentColor = `rgba(${enc.numToRbga[currentColorNum] })`
 
-        if (this.getColor() != currentColor){
-          this.setColor(currentColor)
-        } 
+//         if (this.getColor() != currentColor){
+//           this.setColor(currentColor)
+//         } 
 
-        this.context.fillRect(i, j, this.state.pixelSize, this.state.pixelSize);
+//         this.context.fillRect(i, j, this.state.pixelSize, this.state.pixelSize);
 
 
-      }
-    }
-  }
-  renderPallate(){
-    return <div id='palatte-container'>
-     {enc.numToRbga.map((c, i)=>{
-      const color = `rgba(${c})` 
-      return <div onClick={()=>{this.setColor(color)}}className='pallate-button' key={i} style={{backgroundColor: color}}>
-        </div>
-    })
-  }
-    </div>
+//       }
+//     }
+//   }
+//   renderPallate(){
+//     return <div id='palatte-container'>
+//      {enc.numToRbga.map((c, i)=>{
+//       const color = `rgba(${c})` 
+//       return <div onClick={()=>{this.setColor(color)}}className='pallate-button' key={i} style={{backgroundColor: color}}>
+//         </div>
+//     })
+//   }
+//     </div>
     
-  }
+//   }
 
 
 
   render() {
     return (
       <div>
-        {this.renderPallate()}
-      <canvas id='can' onClick={this.draw} ref={(canvas)=> this.canvas = canvas } width={this.state.width} height={this.state.width}>
-      </canvas>
+      <Canvas/>
       <div onClick={this.test}>test</div>
       <div onClick={this.test2}>test2</div>
       <div>size{this.state.pixelSize}</div>
